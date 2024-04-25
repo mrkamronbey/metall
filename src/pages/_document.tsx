d0d44b9616f73e3e3fc7bb946ad1f961ff0b255e
SoftwareSystemPro <owenmichael0117@gmail.com>
2024-04-25 16:06:41 +0500

    pushg verification files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,12 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta httpEquiv="Content_Type" content="text/html" charSet="UTF-8" />
+        <meta name="google-site-verification"  content="EVSoAgA6tcA0NDW1cEVogQAPdomutUQK9uAHMPjzNIc"/>
+        <meta name="yandex-verification"  content="92a6c68f0aab0eac"/>
         <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
           rel="stylesheet" />
       </Head>
       <body className={unbounded.className}>
-        Verification: 92a6c68f0aab0eac
         Verification: 91fab66f8d2b49f7
         <Main />
         <NextScript />
